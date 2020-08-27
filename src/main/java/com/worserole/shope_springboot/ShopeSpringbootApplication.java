7e5fbd28c4d74a7cc11fc975a7aa8690e55fd3a2
WorseRole <worserole@qq.com>
2020-08-27 17:30:23 +0800

    new project

---
+++ src/main/java/com/worserole/shope_springboot/ShopeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.worserole.shope_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopeSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/worserole/shope_springboot/ShopeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worserole.shope_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
